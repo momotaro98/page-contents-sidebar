fc382b2419cf30805a13658cf775718ec94266ad
Shintaro Ikeda <ikenshirogivenup98@gmail.com>
2017-02-18 02:48:27 +0900

    first commit

---
+++ gulpfile.babel.js
@@ -0,0 +1,87 @@
+import gulp from 'gulp'
+import {merge} from 'event-stream'
+import map from 'map-stream'
+const $ = require('gulp-load-plugins')();
+
+
+// Tasks
+gulp.task('clean', () => {
+  return pipe('./tmp', $.clean());
+});
+
+gulp.task('build', (cb) => {
+  $.runSequence('clean', 'style', 'template', 'js', 'devdist', cb);
+});
+
+gulp.task('default', ['build'], () => {
+  gulp.watch(['./libs/**/*', './src/**/*'], ['default']);
+});
+
+gulp.task('style', () => {
+  return pipe(
+    './src/styles/mdisviewer.less',
+    $.plumber(),
+    $.less({relativeUrls: true}),
+    $.autoprefixer({cascade: true}),
+    './tmp'
+  );
+});
+
+gulp.task('template', () => {
+  const LOTS_OF_SPACES = new Array(500).join(' ');
+
+  return pipe(
+    './src/template.html',
+    $.replace('__SPACES__', LOTS_OF_SPACES),
+    html2js('const TEMPLATE = \'$$\''),
+    './tmp'
+  );
+});
+
+gulp.task('js', ['template'], () => {
+  const src = [
+    './tmp/template.js',
+  ].concat(['./src/config/overrides.js'])
+   .concat('./src/mdisviewer.js');
+
+  return pipe(
+    src,
+    $.babel(),
+    $.concat('mdisviewer.js'),
+    './tmp'
+  );
+});
+
+gulp.task('devdist', ['js'], () => {
+  return merge(
+    pipe('./icons/**/*', './tmp/app/icons'),
+    pipe(['./libs/**/*', './tmp/mdisviewer.*', './src/config/manifest.json'], './tmp/app/'),
+    pipe('./src/config/background.js', $.babel(), './tmp/app/')
+  );
+});
+
+// Helpers
+function pipe(src, ...transforms) {
+  return transforms.reduce((stream, transform) => {
+    const isDest = typeof transform === 'string';
+    return stream.pipe(isDest ? gulp.dest(transform) : transform);
+  }, gulp.src(src));
+}
+
+function html2js(template) {
+  return map(escape);
+
+  function escape(file, cb) {
+    const path = $.util.replaceExtension(file.path, '.js');
+    const content = file.contents.toString();
+    const escaped = content
+      .replace(/\\/g, "\\\\")
+      .replace(/'/g, "\\'")
+      .replace(/\r?\n/g, "\\n' +\n    '");
+    const body = template.replace('$$', escaped);
+
+    file.path = path;
+    file.contents = new Buffer(body);
+    cb(null, file);
+  }
+}
---
+++ src/config/background.js
@@ -0,0 +1,45 @@
+chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
+  if (changeInfo.status !== 'loading') return;
+
+  chrome.tabs.executeScript(tabId, {
+    code: 'var injected = window.mdisviewerInjected; window.mdisviewerInjected = true; injected;',
+    runAt: 'document_start'
+  }, (res) => {
+    if (chrome.runtime.lastError || // don't continue if error (i.e. page isn't in permission list)
+        res[0]) // value of `injected` above: don't inject twice
+      return;
+
+    const cssFiles = [
+      'mdisviewer.css'
+    ];
+
+    const jsFiles = [
+      'mdisviewer.js'
+    ];
+
+    eachTask([
+      (cb) => eachItem(cssFiles, inject('insertCSS'), cb),
+      (cb) => eachItem(jsFiles, inject('executeScript'), cb)
+    ]);
+
+    function inject(fn) {
+      return (file, cb) => {
+        chrome.tabs[fn](tabId, { file: file, runAt: 'document_start' }, cb);
+      };
+    }
+  });
+});
+
+function eachTask(tasks, done) {
+  (function next(index = 0) {
+    if (index === tasks.length) done && done();
+    else tasks[index](() => next(++index));
+  })();
+}
+
+function eachItem(arr, iter, done) {
+  const tasks = arr.map((item) => {
+    return (cb) => iter(item, cb);
+  });
+  return eachTask(tasks, done);
+}
