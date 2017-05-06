556a67fe0979049100d8a4dccb40b5948e4a36c8
Shintaro Ikeda <ikenshirogivenup98@gmail.com>
2017-05-06 15:59:29 +0900

    set process flow and implement header of the sidebar

--- src/view.index.js
+++ src/view.index.js
@@ -3,5 +3,42 @@ class IndexView {
     this.store = store;
     this.adapter = adapter;
     this.$view = $dom.find('.mdisviewer_indexview');
+    this.$body = this.$view.find('.mdisviewer_body');
+  }
+
+  show(path) {
+    this.adapter.loadIndexList(path, (err, indexList) => {
+      if (err) {
+        return;
+      }
+      // this._prepare_show(indexList);
+    });
+    this._showHeader(path);
+    this._showBody();
+  }
+
+  _showHeader(path) {
+    this.$view.find('.mdisviewer_header')
+      .html(
+        '<div class="mdisviewer_header_title">' +
+          '<a href="' + path + '">' + 'demo-link' + '</a>' +
+        '</div>'
+      );
+
+    var $header = this.$view.find('mdisviewer_header');
+    console.log("$header:", $header);
+  }
+
+  _showBody() {
+    this.$view.find('.mdisviewer_body')
+      .html(
+        '<div class="mdisviwer_content">' +
+          '<ul>' +
+            '<li>AAA</li>' +
+            '<li>BBB</li>' +
+            '<li>CCC</li>' +
+          '</ul>' +
+        '</div>'
+      );
   }
 }
